5fa683ec0aa3158bb0909d383c2dbff85f4c4ce2
vicfriedman <victoria.m.friedman@gmail.com>
2016-02-19 17:03:52 -0500

    tests updates issue #6

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -3,15 +3,16 @@ require 'rails_helper'
 RSpec.describe SessionsController, type: :controller do
   describe 'get create' do
     it 'assigns omniauth data to @auth' do
-      auth = {      
-        here: 'is',
-        some: 'auth',
-        data: '.',
-        provider: 'facebook',
+      auth_data = { 
+        'uid' => 1,     
+        'info' => {
+          email: 'Roberto@test.com',
+          name: 'Roberto',
+        }
       }
-      @request.env['omniauth.auth'] = auth
+      @request.env['omniauth.auth'] = auth_data
       get :create
-      assert assigns(:auth) == auth
+      expect(subject.auth).to eq(auth_data)
     end
   end
 end
